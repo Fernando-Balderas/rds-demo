b2db2de1102608261bd559e51f7c415c784662a9
Fernando Balderas Guzmán <fernando.bguzman@gmail.com>
2022-07-20 23:20:56 +0300

    Add prod script

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,21 +1,7 @@
 import errorHandler from 'errorhandler'
-// import mongoose from 'mongoose'
 
 import app from './app'
-import { MONGODB_URI } from './util/secrets'
 import logger from './util/logger'
-
-const mongoUrl = MONGODB_URI
-
-// import { Sequelize } from "sequelize";
-// export const sequelize = new Sequelize(
-//   "postgres",
-//   "postgres",
-//   "mysecretpassword",
-//   {
-//     dialect: "postgres",
-//   }
-// );
 
 import { Sequelize } from 'sequelize-typescript'
 export const sequelize = new Sequelize({
@@ -39,33 +25,13 @@ try {
   process.exit(1)
 }
 
-// sequelize.sync();
-
 ;(async () => {
-  await sequelize.sync({ force: true })
-  // Code here
+  await sequelize.sync()
+  // await sequelize.sync({ force: true })
 })()
 
-// mongoose
-//   .connect(mongoUrl, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useFindAndModify: false,
-//     useCreateIndex: true,
-//   })
-//   .then(() => {
-//     logger.info('Connected to MongoDB')
-//   })
-//   .catch((err: Error) => {
-//     console.log(
-//       'MongoDB connection error. Please make sure MongoDB is running. ' + err
-//     )
-//     process.exit(1)
-//   })
-
 /**
- * Error Handler. Provides error handing middleware
-   only use in development
+ * Error Handler. Provides error handing middleware only use in development
  */
 if (process.env.NODE_ENV === 'development') {
   app.use(errorHandler())
